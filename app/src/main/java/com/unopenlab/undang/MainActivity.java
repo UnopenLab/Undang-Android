eb0883b69906c53b79e9ff768df97ba654439c40
Keunsol <keunsol8247@naver.com>
2017-12-12 23:25:37 +0900

    * Add MainWebview

--- app/src/main/java/com/unopenlab/undang/MainActivity.java
+++ app/src/main/java/com/unopenlab/undang/MainActivity.java
@@ -2,12 +2,19 @@ package com.unopenlab.undang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView mMainWebView;
+    private static final String MAIN_URL = "https://raw.githubusercontent.com/UnopenLab/Undang-Web/master/index.html";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMainWebView = (WebView) findViewById(R.id.wv_main);
+        mMainWebView.loadUrl(MAIN_URL);
     }
 }
